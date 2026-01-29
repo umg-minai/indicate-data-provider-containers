21f1fe0cbd33c23f989a092126bccc46c02cd204
Jan Moringen <jan.moringen@med.uni-greifswald.de>
2026-01-29 11:43:37 +0100

    database: Remove redundant information in 6_custom_concepts.sql

--- database/sql/6_custom_concepts.sql
+++ database/sql/6_custom_concepts.sql
@@ -11,13 +11,13 @@ INSERT INTO cds_cdm.vocabulary
 -- Insert custom concepts
 
 INSERT INTO cds_cdm.concept
-         (concept_id,    concept_code,      domain_id,     vocabulary_id, concept_class_id, concept_name,                                                             valid_start_date, valid_end_date)
-  VALUES (2_000_000_101, 'person-24h-qi01', 'Observation', 'INDICATE',    'Observation',    'Person-level, 24-hour period result for QI 01 Ventilation',              '2026-01-01',     '2099-12-31'),
-         (2_000_000_102, 'person-24h-qi02', 'Observation', 'INDICATE',    'Observation',    'Person-level, 24-hour period result for QI 02 Weaning',                  '2026-01-01',     '2099-12-31'),
-         (2_000_000_103, 'person-24h-qi03', 'Observation', 'INDICATE',    'Observation',    'Person-level, 24-hour period result for QI 03 Feeding',                  '2026-01-01',     '2099-12-31'),
-         (2_000_000_104, 'person-24h-qi04', 'Observation', 'INDICATE',    'Observation',    'Person-level, 24-hour period result for QI 04 Glucose Control',          '2026-01-01',     '2099-12-31'),
-         (2_000_000_105, 'person-24h-qi05', 'Observation', 'INDICATE',    'Observation',    'Person-level, 24-hour period result for QI 05 Thrombembolic Prevention', '2026-01-01',     '2099-12-31'),
-         (2_000_000_106, 'person-24h-qi06', 'Observation', 'INDICATE',    'Observation',    'Person-level, 24-hour period result for QI 06 Infection Rate',           '2026-01-01',     '2099-12-31'),
-         (2_000_000_107, 'person-24h-qi07', 'Observation', 'INDICATE',    'Observation',    'Person-level, 24-hour period result for QI 07 Pain Sedation Delir',      '2026-01-01',     '2099-12-31'),
-         (2_000_000_108, 'person-24h-qi08', 'Observation', 'INDICATE',    'Observation',    'Person-level, 24-hour period result for QI 08 Advanced Care Planing',    '2026-01-01',     '2099-12-31'),
-         (2_000_000_109, 'person-24h-qi09', 'Observation', 'INDICATE',    'Observation',    'Person-level, 24-hour period result for QI 09 Mobilisation',             '2026-01-01',     '2099-12-31');
+         (concept_id,    concept_code, domain_id,     vocabulary_id, concept_class_id, concept_name,                                valid_start_date, valid_end_date)
+  VALUES (2_000_000_101, 'qi01',       'Observation', 'INDICATE',    'Observation',    'Result for QI 01 Ventilation',              '2026-01-01',     '2099-12-31'),
+         (2_000_000_102, 'qi02',       'Observation', 'INDICATE',    'Observation',    'Result for QI 02 Weaning',                  '2026-01-01',     '2099-12-31'),
+         (2_000_000_103, 'qi03',       'Observation', 'INDICATE',    'Observation',    'Result for QI 03 Feeding',                  '2026-01-01',     '2099-12-31'),
+         (2_000_000_104, 'qi04',       'Observation', 'INDICATE',    'Observation',    'Result for QI 04 Glucose Control',          '2026-01-01',     '2099-12-31'),
+         (2_000_000_105, 'qi05',       'Observation', 'INDICATE',    'Observation',    'Result for QI 05 Thrombembolic Prevention', '2026-01-01',     '2099-12-31'),
+         (2_000_000_106, 'qi06',       'Observation', 'INDICATE',    'Observation',    'Result for QI 06 Infection Rate',           '2026-01-01',     '2099-12-31'),
+         (2_000_000_107, 'qi07',       'Observation', 'INDICATE',    'Observation',    'Result for QI 07 Pain Sedation Delir',      '2026-01-01',     '2099-12-31'),
+         (2_000_000_108, 'qi08',       'Observation', 'INDICATE',    'Observation',    'Result for QI 08 Advanced Care Planing',    '2026-01-01',     '2099-12-31'),
+         (2_000_000_109, 'qi09',       'Observation', 'INDICATE',    'Observation',    'Result for QI 09 Mobilisation',             '2026-01-01',     '2099-12-31');
